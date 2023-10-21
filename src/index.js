88c7bf9e914ab85377383df41cb8ddd84f5f9680
sai8151 <saikiranreddy8151@gmail.com>
2023-10-21 17:20:40 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+function BookList() {
+  return (
+    <div>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Book() {
+  return (
+    <section>
+      <Image />
+      <Title />
+      <Author />
+      <Price />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Title() {
+  return <h1>book title</h1>;
+}
+
+function Image() {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/51qihDDNLFL._SY425_.jpg"
+      alt=""
+    />
+  );
+}
+
+function Author() {
+  return <h4>by Jon Gordon and Ken Blanchard</h4>;
+}
+
+function Price() {
+  return <h4>299.00</h4>;
+}
+ReactDOM.render(<BookList />, document.getElementById("root"));
